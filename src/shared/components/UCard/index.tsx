4b5c277c854027c850193169ab59bf44a9426aea
Pedro-Afonso <pedro.766@hotmail.com>
2022-12-11 09:11:25 -0300

    feat: Add grow animation

--- src/shared/components/UCard/index.tsx
+++ src/shared/components/UCard/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useRef, useState } from "react";
 import {
   Button,
   Card,
@@ -5,6 +6,7 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  Grow,
   Paper,
   Typography,
 } from "@mui/material";
@@ -20,22 +22,49 @@ interface ICard {
 /* #1A2632 */
 
 export const UCard: React.FC<ICard> = ({ title, thumbnail, github, url }) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const refCard = useRef<HTMLDivElement>(null);
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(([entry]) =>
+        setIsIntersecting(entry.isIntersecting)
+      ),
+    []
+  );
+
+  useEffect(() => {
+    if (!refCard.current) return;
+    observer.observe(refCard.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [refCard, observer]);
+
   return (
-    <Card component={Paper} variant="elevation">
-      <CardActionArea href={url} target="_blank">
-        <CardMedia component="img" height="120" image={thumbnail} alt={title} />
-        <CardContent>
-          <Typography>{title}</Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button href={github} target="_blank" fullWidth>
-          Repositório
-        </Button>
-        <Button href={url} target="_blank" fullWidth>
-          Visitar
-        </Button>
-      </CardActions>
-    </Card>
+    <Grow in={isIntersecting} timeout={1000}>
+      <Card ref={refCard} component={Paper} variant="elevation">
+        <CardActionArea href={url} target="_blank">
+          <CardMedia
+            component="img"
+            height="120"
+            image={thumbnail}
+            alt={title}
+          />
+          <CardContent>
+            <Typography>{title}</Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button href={github} target="_blank" fullWidth>
+            Repositório
+          </Button>
+          <Button href={url} target="_blank" fullWidth>
+            Visitar
+          </Button>
+        </CardActions>
+      </Card>
+    </Grow>
   );
 };
